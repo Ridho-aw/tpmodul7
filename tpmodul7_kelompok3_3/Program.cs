8ab9925de43c05735ac261e61201838554116543
Ridho Ananta Wibowo <r9480.a2b@gmail.com>
2026-04-05 13:14:52 +0700

    penambahan class program

--- tpmodul7_kelompok3_3/Program.cs
+++ tpmodul7_kelompok3_3/Program.cs
@@ -3,8 +3,29 @@ using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
+public class namaLengkap
+{
+    [JsonPropertyName("Nama Depan")]
+    public string namaDepan { get; set; }
 
+    [JsonPropertyName("Nama Belakang")]
+    public string namaBelakang { get; set; }
+}
+public class DataMahasiswa103082400024
+{
+    public namaLengkap Nama { get; set; }
+    public string nim { get; set; }
+    public string fakultas { get; set; }
 
+    public static void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("tp7_1_103082400024.json");
+
+        DataMahasiswa103082400024 data = JsonSerializer.Deserialize<DataMahasiswa103082400024>(jsonString);
+
+        Console.WriteLine($"Nama {data.Nama.namaDepan} {data.Nama.namaBelakang} dengan nim {data.nim} dari fakultas {data.fakultas}");
+    }
+}
 public class MataKuliah
 {
     [JsonPropertyName("code")]
@@ -28,9 +49,13 @@ public class KuliahMahasiswa103082400024
             Console.WriteLine($"MK {i+1} {data.mk[i].kode} dengan nama mata kuliah {data.mk[i].Nama}");
         }
     }
+}
 
+public class Program
+{
     public static void Main(string[] args)
     {
-        ReadJSON();
+        DataMahasiswa103082400024.ReadJSON();
+        KuliahMahasiswa103082400024.ReadJSON();
     }
 }
